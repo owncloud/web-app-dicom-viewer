33e3967503bab2521028984446a49cb6dac44c8f
Kathrin Koebel <kathrin@koebel.ch>
2023-12-21 15:13:20 +0545

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from '@ownclouders/extension-sdk'
+import vue from '@vitejs/plugin-vue'
+
+/// <reference types="vitest" />
 
 export default defineConfig({
   server: {
     port: 9999
+  },
+  plugins: [vue()],
+  test: {
+    globals:true, 
   }
 })
+
+
+
+
+
