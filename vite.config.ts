3f2eb1a440c36d30fecd7a1a0e43e491e98f747c
Kathrin Koebel <kathrin@koebel.ch>
2023-12-21 15:22:55 +0545

    adjust config again

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from '@ownclouders/extension-sdk'
-import vue from '@vitejs/plugin-vue'
-
-/// <reference types="vitest" />
 
 export default defineConfig({
   server: {
     port: 9999
-  },
-  plugins: [vue()],
-  test: {
-    globals:true, 
   }
 })
 
