a5516be533f0037d51ab9a901f8cfe6fc27a5868
Kathrin Koebel <kathrin@koebel.ch>
2024-01-22 15:02:42 +0545

    put VIP Metadata Overlay in separate component

---
+++ src/components/VipMetadataOverlay.vue
@@ -0,0 +1,50 @@
+<template>
+    <div
+          id="dicom-viewer-vip-metadata"
+          class="oc-position-absolute"
+        >
+          <div class="oc-pr-s oc-font-semibold">
+            <span>{{ patientName || 'patient name not defined' }}</span>
+            <span>
+              (*{{ patientBirthdate || 'birthdate not defined'
+              }})</span
+            >
+          </div>
+          <div class="oc-pr-s oc-font-semibold">
+            <span>{{ institutionName || 'institution name not defined' }}</span
+            >,
+            <span> {{ instanceCreationDateTime || 'instance creation date and time not defined'
+            }}</span>
+          </div>
+        </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useGettext } from 'vue3-gettext'
+
+export default defineComponent({
+  name: 'MetadataSidebar',
+  props: {
+    patientName: {
+      type: String,
+      required: false
+    }, 
+    patientBirthdate: {
+      type: String,
+      required: false
+    }, 
+    institutionName: {
+      type: String,
+      required: false
+    }, 
+    instanceCreationDateTime: {
+      type: String,
+      required: false
+    }
+  },
+  setup(props, { emit }) {
+    //const { $gettext } = useGettext()
+  }
+})
+</script>
