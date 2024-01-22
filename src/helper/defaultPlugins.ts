b91084c94f32b3d5bc757ebcdb449c622e9f7b58
Kathrin Koebel <kathrin@koebel.ch>
2024-01-22 12:22:48 +0545

    remove unused import

--- src/helper/defaultPlugins.ts
+++ src/helper/defaultPlugins.ts
@@ -3,7 +3,6 @@ import { createGettext } from 'vue3-gettext'
 import { h } from 'vue'
 import { abilitiesPlugin } from '@casl/vue'
 import { createMongoAbility, SubjectRawRule } from '@casl/ability'
-// import { AbilityRule } from '@ownclouders/web-client/src/helpers/resource/types' //'web-client/src/helpers/resource/types' 
 import { createPinia } from 'pinia'
 
 
