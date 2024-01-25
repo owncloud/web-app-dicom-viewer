91148edf018a741f0b8fcf4a77367930eb6ddd59
Kathrin Koebel <kathrin@koebel.ch>
2024-01-25 15:53:37 +0545

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ export default defineWebApplication({
         component: AppWrapperRoute(App, {
           applicationId: 'dicom-viewer',
           urlForResourceOptions: {
-            //disposition: 'inline'
+            disposition: 'inline'
           }
         }),
         name: 'dicom-viewer',
