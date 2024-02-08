f3702e6eb7aab01cb3df76ec699835d06b4ed900
Kathrin Koebel <kathrin@koebel.ch>
2024-02-08 18:16:18 +0545

    adding more content to dicomTags.ts

--- src/helper/dicomTags.ts
+++ src/helper/dicomTags.ts
@@ -10,8 +10,18 @@ const dicomTags = {
   'x00100020': 'patientID',
   'x00100040': 'patientSex',
   'x00101030': 'patientWeight',
-  //'': '',
-  //'': '',
+  'x00081030': 'studyDescription',
+  'x00181030': 'protocolName',
+  'x00080050': 'accessionNumber',
+  'x00200010': 'studyID',
+  'x00080020': 'studyDate',
+  'x00080030': 'studyTime',
+  'x0008103e': 'seriesDescription',
+  'x00200011': 'seriesNumber',
+  'x00080060': 'modality',
+  'x00180015': 'bodyPart',
+  'x00080021': 'seriesDate',
+  'x00080031': 'seriesTime',
 }
 
 export default dicomTags
