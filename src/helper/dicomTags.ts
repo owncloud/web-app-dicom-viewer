bde52ab5624e44b2ec0facadab66789cb05e2217
Kathrin Koebel <kathrin@koebel.ch>
2024-02-19 10:59:05 +0545

    remove duplicates from dicom tags

--- src/helper/dicomTags.ts
+++ src/helper/dicomTags.ts
@@ -4,9 +4,6 @@
 const dicomTags = {
   'x00100010': 'patientName',
   'x00100030': 'patientBirthdate',
-  'x00080080': 'institutionName',
-  'x00080012': 'instanceCreationDate',
-  'x00080013': 'instanceCreationTime',
   'x00100020': 'patientID',
   'x00100040': 'patientSex',
   'x00101030': 'patientWeight',
@@ -91,9 +88,6 @@ const dicomTags = {
   'x00201040': 'positionReferenceIndicator',
   'x00281050': 'windowCenter',
   'x00281051': 'windowWidth',
-  '': '',
-  '': '',
-  '': '',
 }
 
 export default dicomTags
