e4b8ade5e60f30939e8842cb145accdc9d7e18ab
Prajwol Amatya <prajwolamatya11@gmail.com>
2024-04-10 16:22:56 +0545

    removed exports of internal functions

--- src/helper/extractMetadata.ts
+++ src/helper/extractMetadata.ts
@@ -19,15 +19,15 @@ export const findDicomTagByValue = (value: string): string | undefined => {
   return Object.keys(dicomTags).find((key) => dicomTags[key] === value)
 }
 
-export const formatDateTagChecker = (tag: string): boolean => {
+const formatDateTagChecker = (tag: string): boolean => {
   return tag.endsWith('_formatDate')
 }
 
-export const formatTimeTagChecker = (tag: string): boolean => {
+const formatTimeTagChecker = (tag: string): boolean => {
   return tag.endsWith('_formatTime')
 }
 
-export const addSopTagChecker = (tag: string): boolean => {
+const addSopTagChecker = (tag: string): boolean => {
   return tag.endsWith('_addSOPuids')
 }
 
@@ -69,11 +69,7 @@ export const extractDicomMetadata = async (imageData: object, tags: string[], la
   return extractedData
 }
 
-export const formatDate = (
-  date: string,
-  language: string,
-  dateFormat: DateTime
-): string | undefined => {
+const formatDate = (date: string, language: string, dateFormat: DateTime): string | undefined => {
   // transforming date into a string that is valid for formatDateFromISO ('YYYY-MM-DDTHH:MM:SS')
   // description of input format see https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html, VR Name 'DA'
   // date output format: e.g. DateTime.DATE_MED, DateTime.DATE_SHORT, see https://moment.github.io/luxon/api-docs/index.html
@@ -91,11 +87,7 @@ export const formatDate = (
   return undefined
 }
 
-export const formatTime = (
-  time: string,
-  language: string,
-  timeFormat: DateTime
-): string | undefined => {
+const formatTime = (time: string, language: string, timeFormat: DateTime): string | undefined => {
   // transform time string retrieved from dicom metadata into a string that is valid for formatDateFromISO ('YYYY-MM-DDTHH:MM:SS')
   // description of input format see https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html, VR Name 'TM'
   // time output format: e.g. DateTime.TIME_SIMPLE, DateTime.TIME_24_WITH_SECONDS, see https://moment.github.io/luxon/api-docs/index.html
