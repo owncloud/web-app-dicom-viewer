1acf1ebf007bfd0101481335fcaf3430065da19a
Prajwol Amatya <prajwolamatya11@gmail.com>
2024-04-11 16:15:30 +0545

    added more input data for extractDicomMetaData function

--- tests/unit/helper/extractMetadata.spec.ts
+++ tests/unit/helper/extractMetadata.spec.ts
@@ -30,20 +30,39 @@ describe('Methods', () => {
         string: function (tag: string) {
           switch (tag) {
             case 'x00100010':
-              return 'testPatient'
+              return 'testPatientName'
+            case 'x00100030':
+              return '19010101'
+            case 'x00080030':
+              return '093801'
+            case 'x00080016':
+              return '1.2.840.10008.1.1'
           }
         }
       }
 
       const dicomMetaData = await extractMetadata.extractDicomMetadata(
         imageData,
-        ['patientName', '', undefined, null],
+        [
+          'patientName',
+          'patientBirthdate_formatDate',
+          'studyTime_formatTime',
+          'SOP_ClassUID_addSOPuids',
+          '',
+          'invalidMetaData',
+          undefined,
+          null
+        ],
         'en'
       )
 
       expect(dicomMetaData).toMatchObject([
-        { label: 'patientName', value: 'testPatient' },
-        { label: '', value: undefined }
+        { label: 'patientName', value: 'testPatientName' },
+        { label: 'patientBirthdate', value: 'Jan 1, 1901' },
+        { label: 'studyTime', value: '09:38:01' },
+        { label: 'SOP_ClassUID', value: '1.2.840.10008.1.1 [Verification SOP Class]' },
+        { label: '', value: undefined },
+        { label: 'invalidMetaData', value: undefined }
       ])
     })
   })
