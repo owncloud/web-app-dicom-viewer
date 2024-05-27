3ab47ab4a9634e9b96e0e68efac977e8affa6871
Salipa-Gurung <salipagurung@gmail.com>
2024-05-27 17:32:18 +0545

    enable dynamic app loading

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import translations from '../l10n/translations.json'
 import { AppWrapperRoute, defineWebApplication } from '@ownclouders/web-pkg'
 import App from './App.vue'
 import { useGettext } from 'vue3-gettext'
+import { id as appId } from '../public/manifest.json'
 
 export default defineWebApplication({
   setup() {
     const { $gettext } = useGettext()
-    const appId = 'com.github.owncloud.web.dicom.viewer'
 
     const appInfo = {
       name: $gettext('DICOM Viewer'),
