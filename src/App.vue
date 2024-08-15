508368e181863c0baeb1c2756090e567a7659471
Kathrin Koebel <kathrin@koebel.ch>
2024-08-15 14:51:25 +0545

    figure out how to refresh viewport after changing parallel scale factor

--- src/App.vue
+++ src/App.vue
@@ -260,7 +260,9 @@ export default defineComponent({
 
       // set initial parallel scale factor and render the image (updates every viewport in the rendering engine)
       this.setViewportCameraParallelScaleFactor()
-      this.setZoom(this.currentImageZoom)
+      // this.setZoom(this.currentImageZoom)
+      // TODO: figure out how to refresh viewport (display image with modified parallel scale factor)
+
       this.viewport.render()
 
       // getting image metadata from viewport
